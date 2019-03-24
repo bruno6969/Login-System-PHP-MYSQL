6a804d4f21430649043a807f6505ed59aebb38ae
Messias Martins <35672212+MartinsMessias@users.noreply.github.com>
2019-03-24 13:48:49 -0500

    Mudado para /conn/

--- conexao.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Tenta se conectar ao servidor MySQL
-$conn = mysqli_connect('localhost', 'admin', 'password');
-// Tenta se conectar a um banco de dados MySQL
-$banco = mysqli_select_db($conn,'sistema');
-?>
