eecbfbc2ab45499dd5bcda6a924ff6a5ee609ef6
Messias Martins <35672212+MartinsMessias@users.noreply.github.com>
2019-03-25 01:44:18 -0500

    v3.0

--- rodape.php
+++ rodape.php
@@ -1,7 +1,7 @@
 <footer class="page-footer indigo" style="margin-bottom: 0;">
   <div class="footer-copyright">
   <div class="container">
-    Copyright 2019 &copy <a class="indigo-text text-lighten-3" href="https://github.com/MartinsMessias">Messias Martins
+    Copyright 2019 &copy <a class="indigo-text text-lighten-3" href="https://github.com/MartinsMessias">Messias Martins | William Assís
   </div>
 </footer>
 <!--  Scripts-->
